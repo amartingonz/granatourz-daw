351a1440b87d40b8c4a31fc6f6a119606078e77f
Alejandro <99648035+amartingonz@users.noreply.github.com>
2023-06-09 13:57:08 +0200

    Granatourz v0.1

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -10,7 +10,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="logo.png">
+        <link rel="icon" href="logo.ico">
         <title>GranaTourz</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
