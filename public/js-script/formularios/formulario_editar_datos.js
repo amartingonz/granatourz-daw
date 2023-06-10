106f46d54cc3c5b3c70fd52ac40dd324a7547e67
Alejandro <99648035+amartingonz@users.noreply.github.com>
2023-06-11 00:55:49 +0200

    Granatourz v0.1

--- public/js-script/formularios/formulario_editar_datos.js
+++ public/js-script/formularios/formulario_editar_datos.js
@@ -4,43 +4,42 @@ function validarFormulario_editar_datos() {
   // Obtener los valores de los campos del formulario
   var nombre = formulario.elements['data[nombre]'].value;
   var apellidos = formulario.elements['data[apellidos]'].value;
-  var email = formulario.elements['data[email]'].value;
   var telefono = formulario.elements['data[telefono]'].value;
   var password = formulario.elements['data[password]'].value;
 
   // Validar el campo nombre (solo letras)
   var nombreRegex = /^[a-zA-Z\sáéíóúÁÉÍÓÚüÜñÑ\s]+$/;
   if (!nombreRegex.test(nombre)) {
-    formulario.elements['data[nombre]'].nextElementSibling.textContent = "El campo de nombre solo puede contener letras.";
+    document.getElementById('nombre-error').textContent = "El campo de nombre solo puede contener letras.";
     return false;
   } else {
-    formulario.elements['data[nombre]'].nextElementSibling.textContent = "";
+    document.getElementById('nombre-error').textContent = "";
   }
 
   // Validar el campo apellidos (letras, espacios y tildes)
   var apellidosRegex = /^[a-zA-Z\sáéíóúÁÉÍÓÚüÜñÑ\s]+$/;
   if (!apellidosRegex.test(apellidos)) {
-    formulario.elements['data[apellidos]'].nextElementSibling.textContent = "El campo de apellidos solo puede contener letras, espacios y tildes.";
+    document.getElementById('apellidos-error').textContent = "El campo de apellidos solo puede contener letras, espacios y tildes.";
     return false;
   } else {
-    formulario.elements['data[apellidos]'].nextElementSibling.textContent = "";
+    document.getElementById('apellidos-error').textContent = "";
   }
 
   // Validar el campo teléfono (número de teléfono móvil)
   var telefonoRegex = /^[6-9]\d{8}$/;
   if (!telefonoRegex.test(telefono)) {
-    formulario.elements['data[telefono]'].nextElementSibling.textContent = "Por favor, introduce un número de teléfono móvil válido.";
+    document.getElementById('telefono-error').textContent = "Por favor, introduce un número de teléfono móvil válido.";
     return false;
   } else {
-    formulario.elements['data[telefono]'].nextElementSibling.textContent = "";
+    document.getElementById('telefono-error').textContent = "";
   }
 
   // Validar el campo contraseña (mínimo 6 caracteres)
   if (password.length < 6) {
-    formulario.elements['data[password]'].nextElementSibling.textContent = "La contraseña debe tener al menos 6 caracteres.";
+    document.getElementById('password-error').textContent = "La contraseña debe tener al menos 6 caracteres.";
     return false;
   } else {
-    formulario.elements['data[password]'].nextElementSibling.textContent = "";
+    document.getElementById('password-error').textContent = "";
   }
 
   // Si todas las validaciones pasan, enviar el formulario
